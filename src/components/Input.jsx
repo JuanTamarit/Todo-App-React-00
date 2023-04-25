9be297bd291e1c25c1c2fe79e9807a5ef77e55c3
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-04-25 14:36:03 +0200

    Valor del input accesible
    Meto el texto del input dentro de una variable accesible.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,20 @@
-import React from "react";
 import "./Input.css";
+import React, { useRef } from "react";
 
 function Input() {
+  const inputRef = useRef(null);
   const createNewTask = (event) => {
+    let newTaskText = inputRef.current.value;
     alert("Se hizo clic en el botón");
+    alert(newTaskText);
   };
   return (
     <div className="input__component">
-      <input type="text" placeholder="Introduce aquí una nueva tarea" />
+      <input
+        type="text"
+        placeholder="Introduce aquí una nueva tarea"
+        ref={inputRef}
+      />
       <button className="new-task-button" onClick={createNewTask}>
         Añadir tarea
       </button>
