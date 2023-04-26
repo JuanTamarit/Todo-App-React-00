bf6209ecc6af8d93f3272983a7414a31b18135e1
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-04-26 13:54:21 +0200

    Funcionalidad: Guardar tareas en lista
    No funciona actualmente

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,17 @@
 import "./Input.css";
-import React, { useRef, useContext, useState } from "react";
-import TaskContext from "../TaskContext";
+import React, { useRef, useContext } from "react";
+import { TaskContext } from "../TaskContext";
 
 function Input() {
   const inputRef = useRef(null);
-  let tasksArray = [];
+  const { tasksArray, setTasksArray } = useContext(TaskContext);
+
+  function saveTask() {
+    const newTasksArray = [...tasksArray, inputRef.current.value];
+    setTasksArray(newTasksArray);
+    localStorage.setItem("tasksArray", JSON.stringify(newTasksArray));
+    inputRef.current.value = "";
+  }
 
   return (
     <div className="input__component">
@@ -13,7 +20,7 @@ function Input() {
         placeholder="Introduce una nueva tarea"
         ref={inputRef}
       />
-      <button className="new-task-button" onClick={""}>
+      <button className="new-task-button" onClick={saveTask}>
         Añadir tarea
       </button>
     </div>
