570fe9dabff1a5ba7bafc070bb23dfbeb9eb26c3
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-04-27 02:41:02 +0200

    User Story 1: Completado
    Intento añadir la funcionalidad de tachar la tarea al marcar el checkbox pero todavía no funciona.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,32 @@
-import React from "react";
 import "./Task.css";
+import React, { useState, useRef } from "react";
 
 function Task(props) {
+  const [isChecked, setIsChecked] = useState(false);
+  const labelRef = useRef(null);
+
+  function handleCheckboxChange() {
+    setIsChecked(!isChecked);
+    handleLabelClass();
+  }
+
+  function handleLabelClass() {
+    if (labelRef.current) {
+      labelRef.current.classList.toggle("completed-task", isChecked);
+    }
+  }
+
   return (
     <div className="task">
-      <input type="checkbox" />
-      <label>{props.task}</label>
+      <input
+        type="checkbox"
+        id={props.task.id}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <label htmlFor={props.task.id} ref={labelRef}>
+        {props.task}
+      </label>
     </div>
   );
 }
